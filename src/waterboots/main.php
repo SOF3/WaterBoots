0a4b07c6b32b59c5450cde6221a5884204710206
Mattemmodder <mathewpsutherland@gmail.com>
2018-02-05 16:33:20 +0000

    Update main.php

--- src/waterboots/main.php
+++ src/waterboots/main.php
@@ -20,7 +20,7 @@ class PluginBase extends Listener{
     }
         
   public function onCommand(CommandSender $sender, Command $command) {
-        if(strtolower($command->getName()) == "wb" {
+        if(strtolower($command->getName()) == "wb") {
         if($sender->hasPermission("boots.water")) {
             public function onSneak(PlayerToggleSneakEvent $event); {
                 $commandS = "setblock ~ ~ ~ water";
