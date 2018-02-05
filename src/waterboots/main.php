f4742032a4d0eb566b118a7df2cf342caf2332f2
Mattemmodder <mathewpsutherland@gmail.com>
2018-02-05 11:00:41 +0000

    Update main.php

--- src/waterboots/main.php
+++ src/waterboots/main.php
@@ -1,15 +1,27 @@
 <?php
 namespace WaterBoots;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\utils\Config;
 use pocketmine\event\player\PlayerToggleSneakEvent;
+use pocketmine\Player;
 use pocketmine\command as c;
 use pocketmine\plugin\PluginBase as PB;
+
 class PluginBase extends Listener{
     
+    public function onEnable() {
+        @mkdir($this->getDataFolder());
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array("CMD" => ""));
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+         $this->getLogger()->info(TextFormat::GREEN . "Created by Cat -Discord- ");
+        
+        
   public function onCommand(CommandSender $sender, Command $command) {
         if(strtolower($command->getName()) == "wb" {
         if($sender->hasPermission("boots.water")) {
             public function onSneak(PlayerToggleSneakEvent $event); {
+                
                 
                     } else {
                         $sender->sendMessage(TF::RED . "Incorrect usage!");
