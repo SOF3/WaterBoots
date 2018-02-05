168d9e2ed7bd78f0c76d40f2effec689694c0703
Mattemmodder <mathewpsutherland@gmail.com>
2018-02-05 08:42:33 +0000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,1 +1,17 @@
-pComing soon!
+<?php
+namespace WaterBoots;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command as c;
+use pocketmine\plugin\PluginBase as PB;
+
+class PluginBase extends Listener{
+    
+  public function onCommand(CommandSender $sender, Command $command) {
+        if(strtolower($command->getName()) == "waterboots" {
+        if($sender->hasPermission("boots.water")) {
+          
+                    } else {
+                        $sender->sendMessage(TF::RED . "Incorrect usage!");
+                        return false;
+    }
+}
