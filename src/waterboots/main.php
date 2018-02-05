584e6eb5167ddc5ad1ad0e41c07e802070216af0
Mattemmodder <mathewpsutherland@gmail.com>
2018-02-05 10:43:42 +0000

    Update main.php

--- src/waterboots/main.php
+++ src/waterboots/main.php
@@ -1,14 +1,16 @@
 <?php
 namespace WaterBoots;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerToggleSneakEvent;
 use pocketmine\command as c;
 use pocketmine\plugin\PluginBase as PB;
 class PluginBase extends Listener{
     
   public function onCommand(CommandSender $sender, Command $command) {
-        if(strtolower($command->getName()) == "waterboots" {
+        if(strtolower($command->getName()) == "wb" {
         if($sender->hasPermission("boots.water")) {
-          
+            public function onSneak(PlayerToggleSneakEvent $event); {
+                
                     } else {
                         $sender->sendMessage(TF::RED . "Incorrect usage!");
                         return false;
