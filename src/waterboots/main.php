ffd56178c9df84e55976b76b3c0b30efb8115db4
Mattemmodder <mathewpsutherland@gmail.com>
2018-02-07 15:22:11 +0000

    Update main.php

--- src/waterboots/main.php
+++ src/waterboots/main.php
@@ -13,27 +13,30 @@ use pocketmine\plugin\PluginBase as PB;
 class PluginBase extends Listener{
 	
 	private $wb = [];
-    
-    public function onEnable() {
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-         $this->getLogger()->info(TextFormat::ORANGE . "Created by Cat -Discord- ");
-	    $this->getLogger()->info(TextFormat::GREEN . "WaterBoots and commands have been successfully enabled!");
-	    
-    }
+	
+	public function onEnable() {
+		
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info(TextFormat::ORANGE . "Created by Cat -Discord- ");
+		$this->getLogger()->info(TextFormat::GREEN . "WaterBoots and commands have been successfully enabled!");
+		
+	}
 	
 	public function onToggle(PlayerToggleSneakEvent $event) {
-		if(isset($this->hasWb[$sender->getName()])) { 
-                $commandS = "setblock ~ ~ ~ water";
-		$this->getServer()->dispatchCommand(new CommandSender(), $commandS);
-			  
-		  }
-        
-  public function onCommand(CommandSender $sender, Command $command) {
-        if($sender->hasPermission("boots.water")) {
-	if(strtolower($command->getName()) == "wb") {
-		 $sender->sendMessage(TF::RED . "logged!");
-		 } else {
-                        $sender->sendMessage(TF::RED . "Incorrect usage or privlages!");
-                        return false;
-    }
-}
+		if(isset($this->hasWb[$sender->getName()])) {
+			$commandS = "setblock ~ ~ ~ water";
+			$this->getServer()->dispatchCommand(new CommandSender(), $commandS);
+		
+		}
+		
+		public function onCommand(CommandSender $sender, Command $command) {
+			if($sender->hasPermission("boots.water")) {
+				if(strtolower($command->getName()) == "wb") {
+					$sender->sendMessage(TF::RED . "logged!");
+				} else {
+					$sender->sendMessage(TF::RED . "Incorrect usage or privlages!");
+					return false;
+					
+				}
+				
+			}
