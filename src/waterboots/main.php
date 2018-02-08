37f41c3fe72f57f98fa07b6870079cdd8cad3b88
Mattemmodder <mathewpsutherland@gmail.com>
2018-02-08 10:59:52 +0000

    Update main.php

--- src/waterboots/main.php
+++ src/waterboots/main.php
@@ -13,7 +13,7 @@ use pocketmine\plugin\PluginBase as PB;{
     
     class main extends pluginbase implements listner {
 
-    private $wb = [];
+    private $ws = [];
 
     public function onEnable() {
 
@@ -24,7 +24,7 @@ use pocketmine\plugin\PluginBase as PB;{
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
             if ($sender->hasPermission("boots.water")) {
-                if (strtolower($command->getName()) == "wb") {
+                if (strtolower($command->getName()) == "ws") {
                     $sender->sendMessage(TF::GREEN . "logged! Crouch to spawn water.");
                 } else {
                     $sender->sendMessage(TF::RED . "Incorrect usage or privlages!");
@@ -36,7 +36,7 @@ use pocketmine\plugin\PluginBase as PB;{
     }
     
     public function onToggle(PlayerToggleSneakEvent $event) {
-        if (isset($this->hasWb[$sender->getName()])) {
+        if (isset($this->hasWs[$sender->getName()])) {
             $commands = "setblock ~ ~ ~ water";
             $this->getServer()->dispatchCommand(new CommandSender(), $commands);
             
